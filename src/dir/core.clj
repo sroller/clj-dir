9afe76e36c4b3fa4712164a69d4c887d3ab2265c
Steffen Roller <steffen.roller@gmail.com>
2017-11-28 09:54:19 -0500

    initial commit

---
+++ src/dir/core.clj
@@ -0,0 +1,19 @@
+(ns dir.core
+  (:gen-class))
+
+(defn directory [path]
+     (clojure.java.io/file path))
+
+(defn files [dir]
+  (file-seq (directory dir)))
+
+(defn -main
+  [& dir]
+  (let [dir (or (first dir) ".")]
+  (printf "dir=%s\n" dir)
+  (doseq [file (files dir)]
+    (if (.isDirectory file)
+      (print " <DIR> ")
+      (print "       "))
+
+    (println (.getPath file)))))
